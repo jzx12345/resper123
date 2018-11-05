eec42d30df2a08f4a8e752edd3f505f91c635f5a
zhansan <123456>
2018-11-05 21:54:22 +0800

    xiugai

--- git-test-projiect/src/Firsttest.java
+++ git-test-projiect/src/Firsttest.java
@@ -1,5 +1,6 @@
 public class Firsttest {
     public static void main(String[] args) {
         System.out.println("haohaoxuexi,haohaogongzuo");
+        System.out.println("hahahhaohah");
     }
 }
